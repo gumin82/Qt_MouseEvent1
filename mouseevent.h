2f21111d77ccff505e3c8ebfcbf7ec34ba709a95
JIA-MIN_CHEN <41343133@nfu.edu.tw>
2025-12-29 15:17:43 +0800

    finish

--- mouseevent.h
+++ mouseevent.h
@@ -2,6 +2,9 @@
 #define MOUSEEVENT_H
 
 #include <QMainWindow>
+#include <QMouseEvent>
+#include <QStatusBar>
+#include <QLabel>
 
 class MouseEvent : public QMainWindow
 {
@@ -10,5 +13,13 @@ class MouseEvent : public QMainWindow
 public:
     MouseEvent(QWidget *parent = nullptr);
     ~MouseEvent();
+protected:
+    void mouseDoubleClickEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+private:
+    QLabel *statusLabel;
+    QLabel *MousePosLabel;
 };
 #endif // MOUSEEVENT_H
